4f44247e69b387a04e912c2ff2e99863541a2520
Jarosław Głowienka <jglowienka@gmail.com>
2022-06-27 17:46:05 +0200

    menu item

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var newsRouter = require('./routes/news');
+var quizRouter = require('./routes/quiz');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -19,8 +21,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.path = req.path;
+  next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
